f5886c4a23c77f2e70105a2388e0c86fd2d4678f
Amafuree <r.dmitry.assist@gmail.com>
2022-12-09 18:18:38 +0300

    init commit

---
+++ src/test/java/com/Amafuree/task/TestTaskPulkovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Amafuree.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskPulkovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
